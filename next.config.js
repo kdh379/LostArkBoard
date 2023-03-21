1cd019e765a65b1448e8c49cfa28901943df94cb
kdh379 <ehgus37625@naver.com>
2023-03-21 23:55:54 +0900

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // basePath: "/LostArkProfile",
     reactStrictMode: true,
     typescript: {
         tsconfigPath: "tsconfig.json",
@@ -9,31 +8,6 @@ const nextConfig = {
         ignoreDuringBuilds: true,
     },
     pageExtensions: ["page.tsx", "page.ts"],
-    webpack: (config) => {
-        const rules = config.module.rules
-            .find((rule) => typeof rule.oneOf === "object")
-            .oneOf.filter((rule) => Array.isArray(rule.use));
-
-        rules.forEach((rule) => {
-            rule.use.forEach((moduleLoader) => {
-                if (
-                    moduleLoader.loader !== undefined &&
-                    moduleLoader.loader.includes("css-loader") &&
-                    typeof moduleLoader.options.modules === "object"
-                ) {
-                    moduleLoader.options.modules.getLocalIdent = (
-                        _context,
-                        _local,
-                        localName
-                    ) => {
-                        return localName;
-                    };
-                }
-            });
-        });
-
-        return config;
-    },
 };
 
 module.exports = nextConfig;
